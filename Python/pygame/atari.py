06c014f2a493cb8918b5524db29300c42af26c1b
dmaya02 <andresdamaya02@gmail.com>
2021-10-29 14:10:33 -0500

    Atari music

--- Python/pygame/atari.py
+++ Python/pygame/atari.py
@@ -1,19 +1,32 @@
-'''
-    Developer: Dmaya02
-    Date: 30-sept-2021
-    Description:
-    Desarrollo de la versión 1.0 de un video juego de atari.
-'''
+#############################
+#  Andrés David Maya Getial #
+#     Ing de sistemas 6A    #
+#        cod  i033119       #
+#        VIDEOJUEGOS        #
+#############################
 
 #Importar librerias
 import sys
 import pygame
-
+import time
+import os
 pygame.init()
+
 
 ####################################################################################
 #Classes
 ####################################################################################
+
+#sonidos-music videogame
+
+romperladrillos = pygame.mixer.Sound("music/romper.wav")
+gameover=pygame.mixer.Sound("music/verguenza.wav")
+rebote=pygame.mixer.Sound("music/rebote.wav")
+
+
+pygame.mixer.music.load("music/romper.wav")
+pygame.mixer.music.play(5)
+
 class Ball(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -21,7 +34,8 @@ class Ball(pygame.sprite.Sprite):
         self.rect = self.img_ball.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.centery = HEIGHT / 2
-        self.speed = [5, 5] # []
+        #[0,1]
+        self.speed = [5, 2] # []
     
     def pibot(self):
         #Validate Y !¡
@@ -59,6 +73,10 @@ class Brick(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = position
 
+
+
+
+
 class Wall(pygame.sprite.Group):
     def __init__(self, totalBricks):
         pygame.sprite.Group.__init__(self)
@@ -73,6 +91,42 @@ class Wall(pygame.sprite.Group):
             if posX >= WIDTH :
                 posX = 0
                 posY += brick.rect.height
+
+
+def game_over():
+    msg = "Game Over"
+    text_color = (63, 74, 106)
+    text_style = pygame.font.SysFont('Arial',40) #(Tipo de letra, Tamaño)
+    txt_screen = text_style.render(msg,True,text_color)
+    txt_screen_rect = txt_screen.get_rect()
+    txt_screen_rect.center = [WIDTH/2,HEIGHT/2]
+    screen.blit(txt_screen,txt_screen_rect)
+    pygame.display.flip()
+    print("perdiste")
+
+    
+def set_score():
+    text_color = (255, 255, 255)
+    text_style = pygame.font.SysFont('Arial',40) #(Tipo de letra, Tamaño)
+    txt_screen = text_style.render(str(score).zfill(3),True,text_color)
+    txt_screen_rect = txt_screen.get_rect()
+    txt_screen_rect.topleft = [1,400]
+    screen.blit(txt_screen,txt_screen_rect)
+
+def set_lives():
+    label = "Vidas: "
+    text_color = (255, 255, 255)
+    text_style = pygame.font.SysFont('Arial',20) #(Tipo de letra, Tamaño)
+    text = label + str(player_lives).zfill(1)
+    txt_screen = text_style.render(text,True,text_color)
+    #txt_screen = text_style.render(label + str(player_lives).zfill(1),True,text_color)
+    txt_screen_rect = txt_screen.get_rect()
+    txt_screen_rect.topleft = [500,400]
+    screen.blit(txt_screen,txt_screen_rect)
+
+
+
+
     
 ####################################################################################
 
@@ -95,10 +149,40 @@ pygame.display.set_icon(icon)
 game_clock = pygame.time.Clock()
 pygame.key.set_repeat(20)
 
+print("Menu nivel de juego")
+print("1. Nivel Normal")
+print("2. Nivel Intermedio")
+print("3. Nivel Avanzado")
+print("4. Salir")
+
+status = True
+while status:
+    opt = int (input("Seleccione el nivel: "))
+    if opt >= 1 and opt <= 4:
+        status = False 
+
+
+if opt==1:
+    ladrillos=20
+elif opt==2:
+    ladrillos=100
+elif opt==3:
+    ladrillos=200
+elif opt==4:
+    print("Has salido del juego")
+    os.system("pause")
+    sys.exit()
+else:
+    print("Opcion invalida")
+    os.system("pause")
+    sys.exit()
+              
 
 ball = Ball()
 player = Bar()
-wall = Wall(112)
+wall = Wall(ladrillos)
+score = 0
+player_lives = 2
 
 #Loop (Revisión cíclica de los eventos) => Listener
 while True:
@@ -112,10 +196,58 @@ while True:
         elif event.type == pygame.KEYDOWN :
             player.slide(event)
 
+
     #Call pibot
-    ball.pibot()        
+    ball.pibot()     
+
+    #colisiones entre bola y barra  ( destruir ladrillos )
+    elements=pygame.sprite.spritecollide(ball,wall, False,collided =None)
+    if elements : #mientras hay ladrillos por chocar
+        brick=elements[0]
+        centX =ball.rect.centerx
+
+        if centX < brick.rect.left or centX>brick.rect.right:
+            ball.speed[0] =-ball.speed[0]
+        else :
+            ball.speed[0] =-ball.speed[1]    
+        wall.remove(brick)  
+        romperladrillos.play()
+
+        score = score+1 #score+=1  
+
+
+        
+
+    #llamar la funcion game over caundo la bola toque el piso
+
+    #if ball.rect.bottom >= HEIGHT:
+        #game_over()
+
+    #Restar vida  
+    if ball.rect.bottom >= HEIGHT:
+        player_lives = player_lives-1 #player_lives-=1  
+
+
+    if player_lives == 0:
+        game_over()
+        gameover.play()
+        time.sleep(5)
+        sys.exit()
+
+    
+        
+
+    #cambio de trayectoria de la bola
+    if pygame.sprite.collide_rect(ball,player):#el jugador es la barra
+        ball.speed[1] = -ball.speed[1]
+        rebote.play()    
+   
     #Set background color
-    screen.fill(BG_COLOR)        
+    screen.fill(BG_COLOR)   
+    set_score() 
+   
+
+    set_lives()    
     #Draw de la ball
     screen.blit(ball.img_ball, ball.rect)
     #Draw de la bar
